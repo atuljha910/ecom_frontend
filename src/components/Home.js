55a3d2ff33728757dfac939f578a6f98d28b0ee5
atuljha910 <atuljha910@gmail.com>
2022-11-15 22:30:45 +0530

    code

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div style = {{backgroundColor:'#87CEEB', justifyContent: 'center', alignItems: 'center'}}>
-      <h1 >
-        Want to read books for free?
-      </h1>
-        <h2>
+    <div style = {{backgroundColor:'#87CEEB'}}>        
+        <img className="stretch" src={require('./logo.jpeg')}        
+        height="300"/>
+        <h1 class="container">
+            Want to read books for free?
+        </h1>
+        <h2 class="container">
             You are at the right place.
         </h2>
-        <img src={require('./logo.jpeg')} />
     </div>
   )
 }
